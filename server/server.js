f1a85f63588ca8f6046d41e0c8dc0ada800a1017
laureanray <laureanray>
2018-02-05 15:10:07 +0800

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express');
+const publicPath = path.join(__dirname, '../public');
+let port = process.env.PORT || 3000;
+let app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log(`Server is up and running at port ${port}`);
+}
