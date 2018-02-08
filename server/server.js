e373df0855fc8ebfee2a81c9bb65c28fedf44b66
laureanray <laureanray>
2018-02-08 14:29:28 +0800

    Tweaks

--- server/server.js
+++ server/server.js
@@ -17,19 +17,25 @@ app.use(express.static(publicPath));
 let user = 0;
 // register an event listener
 io.on('connection', (socket) => {
+    clear();
     console.log('New user connected');
+    user = user + 1;
+    console.log(`Connected user: ${user}`);
     socket.on('disconnect', () => {
-        console.log('User disconnected');       
+        clear();
+        user = user - 1;
+        console.log('User disconnected');
+        console.log(`Conneted user: ${user}`);       
     });
 
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user connected'));
     socket.on('createMessage', (newMessage, callback) => {
-        console.log("New message", newMessage);
+     //   console.log("New message", newMessage);
         io.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
         callback('This is from the server');
-    });
-
+    });     
+    
     socket.on('createLocationMessage', (coords) => {
         io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
     });
